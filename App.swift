791ec35798c165707654d9c354b61bef73bed358
dfurry-boilermaker <danielfurry22@gmail.com>
2025-12-06 00:49:36 -0600

    Add initial implementation of stock management app with data models, view models, and SwiftUI views. Includes Firebase mock integration for stock commitment and price simulation.

---
+++ App.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+// This file is the entry point of your SwiftUI application.
+// When creating a new project in Xcode, this file is generated for you.
+
+@main
+struct HoidmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            // This calls your main application view
+            ContentView()
+        }
+    }
+}
---
+++ Hoidma/Hoidma/HoidmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoidmaApp.swift
+//  Hoidma
+//
+//  Created by Daniel Furry on 11/7/25.
+//
+
+import SwiftUI
+
+@main
+struct HoidmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
